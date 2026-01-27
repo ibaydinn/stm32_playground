5f099f8873c36c8ba224767ceb06f324091627cb
İ.Batuhan Aydın <batuhanaydn1998@gmail.com>
2026-01-27 22:19:57 +0300

    check blue led for if code started to run

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
+#include "bsp/board_led.h"
+
+
 
 int main(void)
 {
+    /* Set BLUE LED on at startup. So I can see that code is started to run. */
+    board_led_init();
+    board_led_on(LED_GREEN);
+
     while (1)
     {
-        
+
     }
 
 
